09135a79394378715fc38ee1015f3466e1e9db05
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2019-05-24 12:45:32 +0000

    Fixed additional_params

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -6,7 +6,13 @@ class Shortener::ShortenedUrlsController < ActionController::Base
   def show
     token = ::Shortener::ShortenedUrl.extract_token(params[:id])
     track = Shortener.ignore_robots.blank? || request.human?
-    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params.permit!, track: track)
+    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: additional_params, track: track)
     redirect_to url[:url], status: :moved_permanently
+  end
+
+  private
+
+  def additional_params
+    params.except(:id, :action, :controller).permit!
   end
 end
